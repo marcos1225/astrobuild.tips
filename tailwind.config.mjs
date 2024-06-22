a0472c4e3062d3e4be40981b3cbfda9146661738
Marcos <marcosgonzana18@gmail.com>
2024-06-22 09:17:30 -0600

    [UPDATE] varias clases

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,15 +3,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			animation: {
-			  border: 'background ease infinite',
-			},
-			keyframes: {
-			  background: {
-				'0%, 100%': { backgroundPosition: '0% 50%' },
-				'50%': { backgroundPosition: '100% 50%' },
-			  },
-			},
 		  },
 	},
 	plugins: [],
